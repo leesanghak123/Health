a2d646b7bcded1a10d6844bc2f9dcf0a524423bf
leesanghak123 <emfprhs9947@naver.com>
2025-06-12 13:36:32 +0900

    로컬 환경 세팅

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+# #!/bin/bash
 
-echo "----- 서버 배포 시작 -----"
-cd /home/ubuntu/health
-docker compose down || true
-docker pull 992382367281.dkr.ecr.ap-northeast-2.amazonaws.com/springboot:latest
-docker pull 992382367281.dkr.ecr.ap-northeast-2.amazonaws.com/vue:latest
-docker pull 992382367281.dkr.ecr.ap-northeast-2.amazonaws.com/es:latest
-docker compose up -d
-echo "----- 서버 배포 끝 -----"
+# echo "----- 서버 배포 시작 -----"
+# cd /home/ubuntu/health
+# docker compose down || true
+# docker pull 992382367281.dkr.ecr.ap-northeast-2.amazonaws.com/springboot:latest
+# docker pull 992382367281.dkr.ecr.ap-northeast-2.amazonaws.com/vue:latest
+# docker pull 992382367281.dkr.ecr.ap-northeast-2.amazonaws.com/es:latest
+# docker compose up -d
+# echo "----- 서버 배포 끝 -----"
